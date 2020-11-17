6a9ecd24ea195684890889b09d4d716648c4bf8d
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-11-17 15:39:14 +0100

    Added event scraping

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
+from urllib.request import urlopen
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 def scrap_for(year, month):
     url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok={}&miesiac={}&lang=1".format(year, month)
+    event_selector = "#kalendarz a.active"
+    
+    response = urlopen(url)
+    html = BeautifulSoup(response.read())
+    selected_elements = html.select(event_selector)
+    
+    for e in selected_elements:
+        print(e)
 
-@app.route("/")
-def index():
-    return "MOBILE WEEIA CALENDAR API"
+# @app.route("/")
+# def index():
+#     return "MOBILE WEEIA CALENDAR API"
+# 
+# @app.route("/events")
+# def actual_events():
+#     pass
+# 
+# @app.route("/events/<year>/<month>")
+# def events(year, month):
+#     pass
 
-@app.route("/events")
-def actual_events():
-    pass
-
-@app.route("/events/<year>/<month>")
-def events(year, month):
-    pass
-
-app.run("lan", 8080)
+scrap_for(2020, 10)
+# app.run("lan", 8080)
