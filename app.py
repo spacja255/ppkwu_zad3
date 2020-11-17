e18d9c5326469d1cb4f1db3468f94aa5d0fbff4a
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-11-17 15:17:46 +0100

    Added some methods

--- app.py
+++ app.py
@@ -2,4 +2,21 @@ from flask import Flask
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
 
-print("OK")
+app = Flask(__name__)
+
+def scrap_for(year, month):
+    url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok={}&miesiac={}&lang=1".format(year, month)
+
+@app.route("/")
+def index():
+    return "MOBILE WEEIA CALENDAR API"
+
+@app.route("/events")
+def actual_events():
+    pass
+
+@app.route("/events/<year>/<month>")
+def events(year, month):
+    pass
+
+app.run("lan", 8080)
